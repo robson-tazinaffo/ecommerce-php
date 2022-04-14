8369f0323bb6865f80d0d091217e1c88be927fb7
Robson Tazinaffo <robsontazinaffo@gmail.com>
2022-04-14 04:44:47 -0300

    Criando a classe Page

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \rtidev\Page;
+
+// $app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 	
-	$sql = new rtidev\DB\Sql();
+	// $sql = new rtidev\DB\Sql();
+	// $results = $sql->select("select * from tb_users");
+    // echo json_encode($results);
 
-	$results = $sql->select("select * from tb_users");
-    
-	echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
